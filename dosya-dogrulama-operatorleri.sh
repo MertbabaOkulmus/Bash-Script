97d0806acecd4379a37e6fd30ca211c779c79a1a
mertbabaokulmus <mertbaba.okulmus@hotmail.com>
2021-06-27 00:29:48 +0300

    dosya dogrulama operatorler örnekleri yapıldı

---
+++ dosya-dogrulama-operatorleri.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+echo -e "Dosyanın ismini giriniz:\c"
+
+read dosyaismi
+
+if [ -e $dosyaismi ] # dosya ismi mevcut mu kontrolü -e exist
+then
+    echo "$dosyaismi bulundu"
+
+else 
+    echo "$dosyaismi bulunamadı"
+fi
+
+# Doğrulama operatörleri
+# -e dosya mevcutmu
+# -f dosya mevcutmu ve regular file (okunabilir yazılabilir mi) // mevcut olmayan bir dosyanın regular durumuna bakılırsa direk false döndürür yani düzenli değil der
+# -s dosya içeriği dolumu boş mu true = dolu
+# -d bunun bir kalsör olup olup olmadığına bakıyor
+# -r read  okunabilir mi
+# -w write yazılabilir mi
+# -x executable 
