a80cb25f94e91fd25ced44658f936842f98f745b
mertbabaokulmus <mertbaba.okulmus@hotmail.com>
2021-06-27 21:09:45 +0300

    updated

--- case.sh
+++ case.sh
@@ -14,3 +14,16 @@ case $tur in
     * ) # else durmunu temsil eder bunlardan hiçbiri yazılmadı ise bu kısmı çalıştır
     echo "Geçersiz giriş";;
 esac
+
+read -p "Lütfen bir karakter giriniz: " karakter
+
+case $karakter in
+    [a-z] )
+    echo " $karakter bir harf tir";;
+    [0-9] )
+    echo " $karakter bir rakamdır";;
+    ? )
+    echo " $karakter bir özel karakterdir";;
+    * )
+    echo " $karakter bir katakter değildir";;
+esac
